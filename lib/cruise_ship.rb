1feea420ab4a6bd2726096f18b0886bff75866c7
Katelyn Peterson <katelynrpeterson16@gmail.com>
2020-06-04 13:09:34 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -9,7 +9,8 @@
 def select_winner(passengers)
   passengers.each do |suite, name|
     if suite == :suite_a && name.start_with?("A")
-      puts name
+      winner = name
     end
   end
+  winner 
 end
